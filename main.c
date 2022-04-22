3977015ef01fef88a368ec2cd3b071a1845b255a
Luciano Lima <lucianolima122123@gmail.com>
2022-04-21 21:28:36 -0300

    Funcoes basicas e declaracao de variaveis

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+char memoria[154];
+unsigned int mbr
+             mar,
+             imm,
+             pc,
+             reg[8];
+
+unsigned char ir,
+              ro0,
+              ro1
+              e,
+              l,
+              g;
+
+unsigned int roZeroMask = 0x00E00000;
+unsigned int roOneMask = 0x001C0000;
+
+void busca(unsigned int pc) {}
+
+void decodifica() {}
+
+void executa() {}
+
+int main() {
+    return 0;
+}
